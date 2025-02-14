eabe0b66427c504b08b5ca2680b3a308e992321f
AlirezaMirghasemi <alireza.mirghasemi@gmail.com>
2025-02-14 19:12:24 +0330

    add theme and router and define them

---
+++ src/TodosRouter.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "./components/MainLayout";
+import Todos from "./components/todos/Todos";
+
+const TodosRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+        {
+            index:true,
+            element:<Todos/>
+        }
+    ],
+  },
+]);
+export default TodosRouter;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './App.css'
 import App from './App.jsx'
+import { RouterProvider } from 'react-router/dom'
+import TodosRouter from './TodosRouter.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={TodosRouter}/>
+  </StrictMode>
 )
