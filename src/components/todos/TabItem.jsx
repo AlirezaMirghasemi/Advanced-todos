56786cfa1d0d1b7375011780ebb19668d53eeb40
AlirezaMirghasemi <alireza.mirghasemi@gmail.com>
2025-02-14 20:33:26 +0330

    make task ready for edit

--- src/components/todos/TabItem.jsx
+++ src/components/todos/TabItem.jsx
@@ -19,15 +19,25 @@ const TabItem = ({ activeTab }) => {
       {filteredTasks.map((task) => (
         <li
           key={task.id}
-          className="list-group-item d-flex align-items-center border-0 mb-2 rounded"
+          className="list-group-item d-flex justify-content-between border-0 mb-2 rounded"
           style={{ backgroundColor: "#f4f6f7" }}
         >
-          <input
-            className="form-check-input me-2"
-            type="checkbox"
-            checked={task.done}
-          />
-          {task.done ? <s>{task.text}</s> : task.text}
+          <div className="d-flex align-items-center">
+            <input
+              className="form-check-input me-2"
+              type="checkbox"
+              checked={task.done}
+            />
+            <input type="text" value={task.done} disabled className={`disabled-input ${task.done ? "done" : ""}`} />
+          </div>
+          <div>
+            <button type="button" className="btn btn-outline-warning me-3">
+              Edit
+            </button>
+            <button type="button" className="btn btn-outline-danger ">
+              Delete
+            </button>
+          </div>
         </li>
       ))}
     </ul>
